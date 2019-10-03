4c94021efdfe51e496f194eef24e2d6a93e4c98a
L11K <L11K@users.noreply.github.com>
2019-10-03 14:59:11 -0300

    Update util.js

--- src/util.js
+++ src/util.js
@@ -43,8 +43,8 @@ export function getAnnouncementEmbed(entry, date, upNext = false) {
     description += "\n\n" + (streamLinks.length > 0 ? "Watch: " + streamLinks + "\n\nIt may take some time to appear on the above service(s)" : "No licensed streaming links available");
   }
 
-let Source = (entry.media.source.length > 0 ) ? `Source: ${entry.media.source}` : "N/A"
-let Studio = (entry.media.studios.edges.length > 0 && entry.media.studios.edges[0].node.name) ? `Studio: ${entry.media.studios.edges[0].node.name}` : "No studio found"
+const Source = (entry.media.source.length > 0 ) ? `Source: ${entry.media.source}` : "N/A"
+const Studio = (entry.media.studios.edges.length > 0 && entry.media.studios.edges[0].node.name) ? `Studio: ${entry.media.studios.edges[0].node.name}` : "No Studio found"
 
   return {
     color: entry.media.coverImage.color ? parseInt(entry.media.coverImage.color.substr(1), 16) : 43775,
