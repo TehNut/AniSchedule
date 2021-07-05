699e84f2f6c04b68b9129c79ed14362b2ff7eafe
Nick Ignoffo <nick@tehnut.info>
2021-07-04 18:11:18 -0700

    Fix enum value for releasing status

--- src/commands/CommandWatch.ts
+++ src/commands/CommandWatch.ts
@@ -83,7 +83,7 @@ export default class CommandWatch extends Command {
     }
 
     const media = (await query("query($id: Int!) { Media(id: $id) { id status title { native romaji english } } }", { id: anilistId })).data.Media;
-    if (![ "NOT_YET_RELEASED", "CURRENT" ].includes(media.status)) {
+    if (![ "NOT_YET_RELEASED", "RELEASING" ].includes(media.status)) {
       interaction.reply({
         ephemeral: true,
         content: `${getTitle(media.title, serverConfig.titleFormat)} is not an upcoming or currently airing anime.`
